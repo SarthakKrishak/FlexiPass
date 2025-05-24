36e01e14cf51331dbe3379725e95e9bd48837366
Sarthak <sarthakkrishak1234@gmail.com>
2025-05-24 13:40:37 +0530

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -34,46 +34,48 @@ const App = () => {
   return (
     <div className="w-full h-screen bg-zinc-800 text-white flex flex-col items-center p-8">
       <h1 className="text-3xl">Password Generator</h1>
-      <div className="rounded-lg overflow-hidden flex mt-6">
-        <input
-          type="text"
-          value={password}
-          className="bg-zinc-600 px-20 py-2"
-          placeholder="password"
-          readOnly
-          ref={passwordRef}
-        />
-        <button onClick={copyToClipboard} className="bg-green-500 px-3">
-          Copy
-        </button>
-      </div>
+      <div className="flex flex-col w-1/3 mt-6 bg-red-600">
+        <div className="rounded-lg overflow-hidden flex w-full">
+          <input
+            type="text"
+            value={password}
+            className="bg-zinc-600 py-3 w-3/4 px-4"
+            placeholder="password"
+            readOnly
+            ref={passwordRef}
+          />
+          <button onClick={copyToClipboard} className="bg-green-500 px-6 rounded-r-lg w-1/4">
+            Copy
+          </button>
+        </div>
 
-      <div className="mt-4 bg-zinc-600 px-20 py-2 rounded-lg">
-        <input
-          type="range"
-          min={6}
-          max={50}
-          value={length}
-          className="cursor-pointer"
-          onChange={(e) => setLength(parseInt(e.target.value))}
-        />
-        <label> Length: {length}</label>
+        <div className="mt-4 bg-zinc-600 pl-6 py-3 rounded-lg flex items-center w-full gap-2">
+          <input
+            type="range"
+            min={6}
+            max={50}
+            value={length}
+            className="cursor-pointer"
+            onChange={(e) => setLength(parseInt(e.target.value))}
+          />
+          <label> Length: {length}</label>
 
-        <input
-          className="ml-5"
-          type="checkbox"
-          defaultChecked={numAllowed}
-          onChange={() => setNumAllowed((prev) => !prev)}
-        />
-        <label> Number </label>
+          <input
+            className="ml-5"
+            type="checkbox"
+            defaultChecked={numAllowed}
+            onChange={() => setNumAllowed((prev) => !prev)}
+          />
+          <label> Number </label>
 
-        <input
-          className="ml-5"
-          type="checkbox"
-          defaultChecked={charAllowed}
-          onChange={() => setCharAllowed((prev) => !prev)}
-        />
-        <label> Character</label>
+          <input
+            className="ml-5"
+            type="checkbox"
+            defaultChecked={charAllowed}
+            onChange={() => setCharAllowed((prev) => !prev)}
+          />
+          <label> Character</label>
+        </div>
       </div>
     </div>
   );
